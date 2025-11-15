80bbf8726e28135aa25de5ddd4b85eb061b0b0da
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-11-15 15:41:43 +0530

    second commit

--- src/services/GraphQlClient/gqlclient.ts
+++ src/services/GraphQlClient/gqlclient.ts
@@ -1,3 +1,5 @@
 import { GraphQLClient } from "graphql-request";
-const graphqlClient=new GraphQLClient("http://localhost:3000/api/graphql")
+
+const graphqlClient = new GraphQLClient(`${process.env.NEXT_PUBLIC_BASE_URL}/api/graphql`);
+
 export default graphqlClient;
